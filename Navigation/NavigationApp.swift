884e59746f3841e440c453eb5c80ebf8566efc66
Alonso Acosta <alonso.ae@hotmail.com>
2026-01-27 13:03:12 -0600

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Alonso Acosta Enriquez on 27/01/26.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
