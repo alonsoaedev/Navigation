cf82d4e6b6277739c342c8b63b85ccf6a3024613
Alonso Acosta <alonso.ae@hotmail.com>
2026-01-27 13:26:15 -0600

    Show the issue of using NavigationLink for dynamic data

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    let number: Int
+    
+    var body: some View {
+        Text("Detail for \(number)")
+    }
+    
+    init(number: Int) {
+        self.number = number
+        print("Creating detail view \(number)")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(0..<1000) { i in
+                NavigationLink("Tap me") {
+                    // What will happen is that just by showing the NavigationLink
+                    // on the screen it's enough for SwiftUI to automatically create
+                    // the detail view inside it. This is a problem when we have dynamic data
+                    DetailView(number: i)
+                }
+            }
         }
-        .padding()
     }
 }
 
