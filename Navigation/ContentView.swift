e7e27f6521c4a14cb93ae7f837d72e254b1d35cd
Alonso Acosta <alonso.ae@hotmail.com>
2026-01-27 19:18:12 -0600

    Return to the root view programmatically

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,34 +7,31 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+    @Binding var path: [Int]
+    
+    var body: some View {
+        NavigationLink("Go to random number", value: Int.random(in: 1..<1000))
+            .navigationTitle("Number: \(number)")
+            .toolbar {
+                Button("Home") {
+                    path.removeAll()
+//                    path = NavigationPath() // in case the NavigationPath is used
+                }
+            }
+    }
+}
+
 struct ContentView: View {
-    @State private var path = NavigationPath()
+    @State private var path: [Int] = []
+    
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(0..<5) { i in
-                    NavigationLink("Select number: \(i)", value: i)
+            DetailView(number: 0, path: $path)
+                .navigationDestination(for: Int.self) { number in
+                    DetailView(number: number, path: $path)
                 }
-                
-                ForEach(0..<5) { i in
-                    NavigationLink("Select string: \(i)", value: String(i))
-                }
-            }
-            .toolbar {
-                Button("Push number 21") {
-                    path.append(21)
-                }
-                
-                Button("Push Hello") {
-                    path.append("Hello")
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You sleected the number: \(selection)")
-            }
-            .navigationDestination(for: String.self) { selection in
-                Text("You sleected the string: \(selection)")
-            }
         }
     }
 }
