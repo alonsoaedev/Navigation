79a62905910a9b195fd93d498568c832634a7a15
Alonso Acosta <alonso.ae@hotmail.com>
2026-01-27 21:49:51 -0600

    Save the navigation stack paths

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,6 +7,70 @@
 
 import SwiftUI
 
+@Observable
+class IntPathStore {
+    private let savePath: URL = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    var path: [Int] {
+        didSet {
+            save()
+        }
+    }
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                path = decoded
+                return
+            }
+        }
+        
+        path = []
+    }
+    
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save the navigation data")
+        }
+    }
+}
+
+@Observable
+class NavigationPathPathStore {
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    var path: NavigationPath {
+        didSet {
+            save()
+        }
+    }
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+    }
+    
+    func save() {
+        guard let representation = path.codable else { return }
+        
+        do {
+            let data = try JSONEncoder().encode(representation)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save the navigation data")
+        }
+    }
+}
+
 struct DetailView: View {
     var number: Int
     @Binding var path: [Int]
@@ -24,13 +88,14 @@ struct DetailView: View {
 }
 
 struct ContentView: View {
-    @State private var path: [Int] = []
+//    @State private var path: [Int] = []
+    @State private var pathStore = IntPathStore()
     
     var body: some View {
-        NavigationStack(path: $path) {
-            DetailView(number: 0, path: $path)
+        NavigationStack(path: $pathStore.path) {
+            DetailView(number: 0, path: $pathStore.path)
                 .navigationDestination(for: Int.self) { number in
-                    DetailView(number: number, path: $path)
+                    DetailView(number: number, path: $pathStore.path)
                 }
         }
     }
