8c5c3cf688dda43a76579938e82228055c1c18ca
Alonso Acosta <alonso.ae@hotmail.com>
2026-01-27 18:26:55 -0600

    Navigate views programmaticly

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,41 +7,26 @@
 
 import SwiftUI
 
-struct Student: Hashable {
-    var id = UUID()
-    let name: String
-    let age: Int
-}
-
-struct DetailView: View {
-    let number: Int
-    
-    var body: some View {
-        Text("Detail for \(number)")
-    }
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating detail view \(number)")
-    }
-}
-
 struct ContentView: View {
+    @State private var path: [Int] = []
+    
     var body: some View {
-        NavigationStack {
-            List(0..<1000) { i in
-                NavigationLink("Select \(i)", value: i)
-                NavigationLink("Estudiante", value: Student(name: "Alonso", age: 30))
+        NavigationStack(path: $path) {
+            VStack {
+                Button("Show 32") {
+                    path = [32]
+                }
+                
+                Button("Show 64") {
+                    path.append(64)
+                }
+                
+                Button("Show 32 then 64") {
+                    path = [32, 64]
+                }
             }
             .navigationDestination(for: Int.self) { selection in
-                // Now this view will only created when the NavigationLink attached
-                // to the Integer had been tapped
-                DetailView(number: selection)
-            }
-            .navigationDestination(for: Student.self) { student in
-                // Same here, this view will be loaded only on screen when the NavigationLink
-                // attached to the Student type had been tapped
-                Text("Hello, \(student.name)")
+                Text("You sleected \(selection)")
             }
         }
     }
